da2bb7bae7ce7bc7c0fe98be416782fec212b71e
Atharva Joshi <iamatharvaj@gmail.com>
2021-06-07 11:10:40 +0530

    Added: Solution for use-case-17

--- test/java/AddressBookServiceTest.java
+++ test/java/AddressBookServiceTest.java
@@ -1,12 +1,21 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import java.util.List; 
+import java.util.List;
 
 public class AddressBookServiceTest {
     @Test
     void givenContactsWhenAdded_ShouldReturnTheTotalCount() {
         AddressBookService addressBookService = new AddressBookService();
-        List<AddressBookContacts> addressBookContacts = addressBookService.readAddressBookContacts(AddressBookService.IOService.DB_IO);
+        List<AddressBookContacts> addressBookContacts = addressBookService.readAddressBookData(AddressBookService.IOService.DB_IO);
         Assertions.assertEquals(4, addressBookContacts.size());
+    }
+
+    @Test
+    void givenContactWhenUpdated_ShouldSyncWithDB() {
+        AddressBookService addressBookService = new AddressBookService();
+        List<AddressBookContacts> addressBookContacts = addressBookService.readAddressBookData(AddressBookService.IOService.DB_IO);
+        addressBookService.updateLastName("Ajay", "dhu");
+        boolean result = addressBookService.AddressBookInSyncWithDB("Ajay");
+        Assertions.assertTrue(result);
     }
 }
